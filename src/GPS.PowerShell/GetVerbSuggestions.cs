1c6dc6c48f2be63a91f31085b660acefbfc967ca
The Sharp Ninja <ninja@thesharp.ninja>
2020-07-23 11:48:54 -0500

    Add project files.

---
+++ src/GPS.PowerShell/GetVerbSuggestions.cs
@@ -0,0 +1,26 @@
+﻿using System.Management.Automation;
+
+namespace GPS.PowerShell
+{
+    [Cmdlet(VerbsCommon.Get, "VerbSuggestions")]
+    public class GetVerbSuggestions : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Verb to find a suitable approved Verb to utilize."
+        )]
+        public string Verb{get;set;}
+
+        protected override void BeginProcessing(){
+            base.BeginProcessing();
+        }
+        protected override void ProcessRecord(){
+            base.ProcessRecord();
+        }
+        protected override void EndProcessing(){
+            base.EndProcessing();
+        }
+    }
+}
